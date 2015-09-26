f68f6e954fb6fff5dc64f4617760b8eda5b0e856
Martin Azpillaga <martin.azpillaga@gmail.com>
2015-09-26 21:21:55 +0200

    UI: dynamic buttons and actions
    Te¡he buttons are created refreshing the ingameHUD object, the actions
    for each object are passed to the hud using setActions. Actions are
    delegates of type void name()

---
+++ Team-C/Assets/Scripts/IngameHUD.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public delegate void Action();
+
+public class IngameHUD : MonoBehaviour
+{
+    [SerializeField] private GameObject actionPanel;
+
+    [SerializeField] private GameObject actionButton;
+
+    private List<Action> actions;
+
+    public void setActions( List<Action> actions)
+    {
+        this.actions = actions;
+    }
+
+    public void refresh()
+    {
+        foreach( Action action in actions )
+        {
+            GameObject actionGO = Instantiate(actionButton) as GameObject;
+            actionGO.transform.SetParent(actionPanel.transform);
+            Action act = action;
+            actionGO.GetComponent<Button>().onClick.AddListener(() => { act(); } );
+            actionGO.GetComponentInChildren<Text>().text = action.ToString();
+            action();
+
+        }
+    }
+
+    public void MyMethod() { print(5); }
+
+
+
+}
--- Team-C/Assets/Scripts/Unit.cs
+++ Team-C/Assets/Scripts/Unit.cs
@@ -1,8 +1,32 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+
+
 
 public class Unit : MonoBehaviour 
 {
-	
+    [SerializeField] private IngameHUD hud;
+
+    void Start()
+    {
+        hud.setActions( new List<Action>() { CreateUnit, DestroyBuilding, Upgrade });
+        hud.refresh();
+    }
+
+    void CreateUnit()
+    {
+        print("create");
+    }
+
+    void DestroyBuilding()
+    {
+        print("destroy");
+    }
+
+    void Upgrade()
+    {
+        print("upgrade");
+    }
 
 
 }
