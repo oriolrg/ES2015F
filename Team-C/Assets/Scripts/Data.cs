f53aeafa3b4ce0728e42d91335b862415f3460c3
Martin Azpillaga <martin.azpillaga@gmail.com>
2015-09-28 20:19:13 +0200

    Data persistance: Settings.asset created
    Settings.asset can be accessed via Editor: Select Data/Create Settings

---
+++ Team-C/Assets/Scripts/Data.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class Data : ScriptableObject 
+{
+    public float UnitHealth     = 100;
+    public float UnitAttack     = 100;
+    public float UnitDefense    = 100;
+    public float UnitMovement   = 100;
+    public float UnitMaximum    = 100;
+}
---
+++ Team-C/Assets/Scripts/Settings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class Settings : MonoBehaviour 
+{
+	[MenuItem("Settings/Edit Settings")]
+    public void EditSettings()
+    {
+        
+    }
+
+}
