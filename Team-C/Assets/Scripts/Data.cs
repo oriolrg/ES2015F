3c98b76c99397cfd8a44bd20934ca41bd3dbedf6
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2015-09-30 14:27:47 +0200

    Serializable Dictionary
    Unity doesn't support dictionary serialization by default, 2 classes
    created by vexe solve the problem.

--- Team-C/Assets/Scripts/Data.cs
+++ Team-C/Assets/Scripts/Data.cs
@@ -2,8 +2,12 @@
 using System;
 
 [Serializable]
+public class StatDictionary : SerializableDictionary<string, float> { }
+
+[Serializable]
 public class Data : ScriptableObject 
 {
+    public StatDictionary stats;
     public float UnitHealth     = 100;
     public float UnitAttack     = 100;
     public float UnitDefense    = 100;
