c3ce4bcf8ed70c81c8e9c2240d7cf13b93da092f
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2015-10-12 00:36:00 +0200

    new UI marble: structure
    All UI elements organized

---
+++ RTS Dev Project/Assets/Scripts/UI/UnfillWithTime.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UnfillWithTime : MonoBehaviour {
+
+	public int time = 5;
+	public float remaining = 5;
+    public Action callback;
+
+	// Use this for initialization
+	void OnEnable () 
+	{
+		remaining = time;
+		Invoke ("Spawn", time);	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (remaining > 0) {
+			remaining -= Time.deltaTime;
+			float percent = remaining / (float)time;
+			GetComponent<Image> ().fillAmount = percent;
+			print (Time.deltaTime);
+		}
+        else
+        {
+            callback();
+            Destroy(gameObject);
+        }
+	}
+}
