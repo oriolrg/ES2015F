9390f1a4addf692d6875d6cb953766e193966b35
sergirovira <srovircifh@gmail.com>
2015-10-18 11:32:10 +0200

    Update A* grid when a building is added

---
+++ RTS Dev Project/Assets/Scripts/Control/updateGrid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class updateGrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("patata");
+		AstarPath.active.Scan();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
