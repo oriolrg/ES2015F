a4314b02bef865255f91e879f7c6082487668124
czs92 <kaz1992@gmail.com>
2015-10-24 17:35:38 +0200

    Model, textures and grid DONE

---
+++ RTS Dev Project/Assets/Scripts/MapInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapInfo : MonoBehaviour {
+
+	public string mapID = "meadow01";
+	public string mapName = "Meadow Map";
+	//public Transform cameraTransform;
+
+
+	// Use this for initialization
+	void Start() {
+		//cameraTransform.transform.position = new Vector3(56,30,-3); 
+		//cameraTransform.transform.rotation = new Quaternion();
+		//cameraTransform.transform.localScale = new Vector3(1,1,1); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
