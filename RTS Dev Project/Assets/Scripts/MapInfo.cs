2ce8df740d78facc86afda927cffd75f3fc36b7e
czs92 <kaz1992@gmail.com>
2015-10-26 18:17:59 +0100

    Include new informatiin on the Script

--- RTS Dev Project/Assets/Scripts/MapInfo.cs
+++ RTS Dev Project/Assets/Scripts/MapInfo.cs
@@ -5,14 +5,15 @@ public class MapInfo : MonoBehaviour {
 
 	public string mapID = "meadow01";
 	public string mapName = "Meadow Map";
-	//public Transform cameraTransform;
-
+	public Transform cameraTransform;
+	public Transform towncenter1;
+	public Transform towncenter2;
+	public Transform towncenter3;
+	public Transform towncenter4;
 
 	// Use this for initialization
 	void Start() {
-		//cameraTransform.transform.position = new Vector3(56,30,-3); 
-		//cameraTransform.transform.rotation = new Quaternion();
-		//cameraTransform.transform.localScale = new Vector3(1,1,1); 
+
 	}
 	
 	// Update is called once per frame
