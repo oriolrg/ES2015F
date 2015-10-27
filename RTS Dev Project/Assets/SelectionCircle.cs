90d0b85bcdcd039c899cc6e4f0caf64c58d69f66
SergiCebrianGres <superlistor@hotmail.com>
2015-10-27 12:20:51 +0100

    Firsc selction circle upgrade

---
+++ RTS Dev Project/Assets/SelectionCircle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionCircle : MonoBehaviour {
+    private Renderer parentRenderer;
+    private Projector projector;
+
+	// Use this for initialization
+	void Start () {
+        parentRenderer = GetComponentInParent<Renderer>();
+        projector = GetComponent<Projector>();
+        if (parentRenderer != null)
+        {
+            if (projector != null)
+            {
+                projector.orthographicSize = parentRenderer.bounds.size.x + 1;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
