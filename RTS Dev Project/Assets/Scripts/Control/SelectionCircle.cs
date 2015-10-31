1647746cec404a8225c3155203667a69da633c15
SergiCebrianGres <superlistor@hotmail.com>
2015-10-31 19:22:32 +0100

    AddSelectedPrefab(GO) added
    Adds the selected projector adjusted for size by codee

--- RTS Dev Project/Assets/Scripts/Control/SelectionCircle.cs
+++ RTS Dev Project/Assets/Scripts/Control/SelectionCircle.cs
@@ -7,19 +7,21 @@ public class SelectionCircle : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        parentRenderer = GetComponentInParent<Renderer>();
-        projector = GetComponent<Projector>();
-        if (parentRenderer != null)
-        {
-            if (projector != null)
-            {
-                projector.orthographicSize = parentRenderer.bounds.size.x + 1;
-            }
-        }
+        init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void init()
+    {
+        parentRenderer = GetComponentInParent<Renderer>();
+        projector = GetComponent<Projector>();
+        if (parentRenderer != null && projector != null)
+        {
+                projector.orthographicSize = parentRenderer.bounds.size.x + 1;
+        }
+    }
 }
