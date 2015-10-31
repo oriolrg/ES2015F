a69397211deacec4117d4f0edcdbca2e63e341a9
Álvaro Parafita <parafita.alvaro@gmail.com>
2015-10-31 12:13:57 +0100

    MainMenu enhancement: model displayer when changing players settings, GameData script to store menu options picked and GameStart method changed to use that info

---
+++ RTS Dev Project/Assets/Scripts/Control/GameInitializer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameInitializer : MonoBehaviour {
+
+	[SerializeField] private GameObject sceneMap; // Map that will be replaced by GameData.map
+	[SerializeField] private GameObject minimap; // To replace variable Ground
+	[SerializeField] private List<GameObject> townCenters; // Town centers that will be erased when Initializing
+
+	void Awake() {
+		GameDataEditorPicker gmep = GetComponent<GameDataEditorPicker>();
+		if (gmep != null) 
+			gmep.AddFakeGameData();
+
+		if (!GameData.sceneFromMenu) {
+			// Don't instantiate anything; only do that when coming from the menu.
+			this.enabled = false;
+			return;
+		}
+
+		// Reallocate mainCamera wherever map tells us
+		Transform newCameraTransform = GameData.map.transform.Find ("Main Camera");
+		Camera.main.transform.position = newCameraTransform.position;
+		Camera.main.transform.rotation = newCameraTransform.rotation;
+	}
+
+	void Start() {
+		// Replace current scene map by GameData.map
+		Destroy(sceneMap);
+		GameObject map = (GameObject) Instantiate (
+			GameData.map, 
+			Vector3.zero, //GameData.map.GetComponent<TerrainCollider>().bounds.extents / 2f, 
+			Quaternion.identity
+		);
+
+		MinimapCamera minimapCamera = minimap.GetComponent<MinimapCamera>();
+		minimapCamera.ground = map;
+		minimapCamera.updateCameraAttributes();
+		//minimapCamera.updateViewport();
+		
+		// Replace current Town Centers by those detailed by GameData and GameData.map
+	}
+}
---
+++ RTS Dev Project/Assets/Scripts/View/Main Menu/PlayersModelsListener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayersModelsListener : ChoicePickerChangeStateListener {
+
+	[SerializeField] private List<GameObject> civilizations;
+	//[SerializeField] private GameObject romans;
+
+	public override void OnChangeState(string state){
+		foreach (GameObject go in civilizations){
+			go.SetActive (go.name.Equals(state));
+		}
+	}
+
+	public override void OnChangeActive(bool active){
+		gameObject.SetActive(active);
+	}
+}
