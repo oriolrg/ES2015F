239a1b5109d5bddc9cf3427bdd2c348e345d20e7
Álvaro Parafita <parafita.alvaro@gmail.com>
2015-11-01 12:17:10 +0100

    Prepared scripts for GameInitializer
    Scripts are prepared, but not active, for GameInitializer for when integration has been done and a stable scene is prepared

--- RTS Dev Project/Assets/Scripts/View/Main Menu/MainMenu.cs
+++ RTS Dev Project/Assets/Scripts/View/Main Menu/MainMenu.cs
@@ -15,12 +15,30 @@ public class MainMenu : MonoBehaviour {
 	[SerializeField] private List<GameObject> maps;
 
 	void Start() {
+//		List<string> mapNames = new List<string>(maps.Count);
+//		foreach (GameObject m in maps)
+//			mapNames.Add(m.GetComponent<MapData>().name);
+//		
+//		map.GetComponent<ChoicePicker>().SetOptions(mapNames);
 	}
 
 	public void NewGame(string scene){
 		GameData.diff = GetEnumValue<GameData.DifficultyEnum>(gameDiff.GetCurrentOption ());
-		//GameData.map = GetEnumValue<GameData.DifficultyEnum>(gameDiff.GetCurrentOption ());
 
+		// Set map
+//		bool mapFound = false;
+//		foreach (GameObject m in maps){
+//			if (m.GetComponent<MapData>().name == map.GetCurrentOption()){
+//				GameData.map = m;
+//				mapFound = true;
+//				break;
+//			}
+//		}
+//
+//		if (!mapFound)
+//			throw new UnityException("Picked map not found in maps list!");
+
+		// Set winConditions
 		GameData.winConditions.Clear ();
 		foreach (TextToggle t in winConditions) {
 			if (t.isActive()){
@@ -28,6 +46,7 @@ public class MainMenu : MonoBehaviour {
 			}
 		}
 
+		// Set civilization of all players and skill of CPUs
 		String civ, skill;
 
 		GameData.player = new GameData.PlayerData (
@@ -54,13 +73,16 @@ public class MainMenu : MonoBehaviour {
 		// Tell GameController that it should initialize game with GameData
 		GameData.sceneFromMenu = true; 
 
+		// Check if all GameConditions are correct
 		if (GameData.GameConditionsCorrect()){
+			// Everything's fine. Start game
 			StartCoroutine( LoadScene(scene) );
 		}
 	}
 	
 	IEnumerator LoadScene(string scene)
 	{
+		// Coroutine that opens the loading screen for 3 seconds and loads the game
 		if (loadingScreen != null){
 			loadingScreen.SetActive(true);
 			yield return new WaitForSeconds (3.0f);
