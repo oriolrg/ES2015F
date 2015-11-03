c75d988493232848a87170c74107c8fb7d83e919
MarcBeltran <beltransegarram@gmail.com>
2015-11-03 20:07:33 +0100

    Added data of civilizations, modfiied prefabs to include identity, spawner and queue

--- RTS Dev Project/Assets/Scripts/Model/DataManager.cs
+++ RTS Dev Project/Assets/Scripts/Model/DataManager.cs
@@ -9,7 +9,9 @@ public class DataManager : MonoBehaviour
 
     public Dictionary<Civilization, List<string>> names = new Dictionary<Civilization, List<string>>()
     {
-        {Civilization.Greeks, new List<string>() { "Agapetus", "Anacletus", "Eustathius", "Helene", "Herodes", "Isidora", "Kosmas", "Lysimachus", "Lysistrata", "Nereus", "Niketas", "Theodoro", "Zephyros" }}
+        {Civilization.Greeks, new List<string>() { "Agapetus", "Anacletus", "Eustathius", "Helene", "Herodes", "Isidora", "Kosmas", "Lysimachus", "Lysistrata", "Nereus", "Niketas", "Theodoro", "Zephyros" }},
+        {Civilization.Egyptians, new List<string>() { "Agapetus", "Anacletus", "Eustathius", "Helene", "Herodes", "Isidora", "Kosmas", "Lysimachus", "Lysistrata", "Nereus", "Niketas", "Theodoro", "Zephyros" }},
+        {Civilization.Neutral, new List<string>() { "Agapetus", "Anacletus", "Eustathius", "Helene", "Herodes", "Isidora", "Kosmas", "Lysimachus", "Lysistrata", "Nereus", "Niketas", "Theodoro", "Zephyros" }}
     };
 
     public List<string> adjectives = new List<string>() { "Important", "Lazy", "Popular", "Historical", "Scared", "Old", "Traditional", "Strong", "Helpful", "Competitive", "Legal", "Obvious" };
