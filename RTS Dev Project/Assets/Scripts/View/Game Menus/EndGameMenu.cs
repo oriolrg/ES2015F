333551e3aa11b5f42327956c019fa93d1dba2c34
Álvaro Parafita <parafita.alvaro@gmail.com>
2015-11-14 18:05:55 +0100

    Add GameMenus - EscMenu and EndMenu, with pause function
    Minimap was edited to consider Time.scaleTime == 0 in Update()

---
+++ RTS Dev Project/Assets/Scripts/View/Game Menus/EndGameMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndGameMenu : MonoBehaviour {
+
+	public string victoryText = "You won!";
+	public string defeatText = "You lost :(";
+
+	public bool victory = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable(){
+		string message;
+
+		if (victory)
+			message = victoryText;
+		else
+			message = defeatText;
+
+		foreach (Transform t in transform) {
+			if (t.gameObject.name.Equals ("Text")){
+				t.gameObject.GetComponent<Text>().text = message;
+				break;
+			}
+		}
+
+		gameObject.SetActive(true);
+
+		Time.timeScale = 0;
+	}
+
+	void OnDisable() { Time.timeScale = 1; }
+}
---
+++ RTS Dev Project/Assets/Scripts/View/Game Menus/GameMenuBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenuBehaviour : MonoBehaviour {
+
+	public string mainMenuSceneName = "Main Menu";
+
+	[SerializeField] private GameObject escGameMenu;
+	[SerializeField] private GameObject endGameMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!endGameMenu.activeSelf && !escGameMenu.activeSelf && Input.GetKey(KeyCode.Escape)){
+			escGameMenu.SetActive(true);
+		}
+			
+	}
+
+	public void ToMainMenu(){
+		Application.LoadLevel (mainMenuSceneName);
+	}
+
+	public void EndGameMenu(bool victory){
+		endGameMenu.GetComponent<EndGameMenu>().victory = victory;
+		endGameMenu.SetActive(true);
+	}
+}
