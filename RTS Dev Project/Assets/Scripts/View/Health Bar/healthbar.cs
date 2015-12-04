9eb752f2d197ee44745901a3639b066526b71eef
laianad <laia_nadal@hotmail.com>
2015-12-04 20:28:24 +0100

    health bar for construction

--- RTS Dev Project/Assets/Scripts/View/Health Bar/healthbar.cs
+++ RTS Dev Project/Assets/Scripts/View/Health Bar/healthbar.cs
@@ -7,6 +7,7 @@ public class healthbar : MonoBehaviour
 
     public float maxHealth;
     public float curHealth;
+    public float auxMaxHealth;
 
     public float minXValue;
     public float maxXValue;
@@ -18,107 +19,92 @@ public class healthbar : MonoBehaviour
     private GameObject g;
     private GameObject g1;
 
-	private LOSEntity unitLOSEntity;
+    private LOSEntity unitLOSEntity;
 
     // Use this for initialization
     void Start()
     {
-        maxHealth = GetComponent<Health>().getMaxHealth();
-        curHealth = GetComponent<Health>().getHealth();
 
-        if (!GetComponent<Identity>().unitType.isBuilding())
+        if (GetComponent<Identity>().unitType.isBuilding())
         {
-            g = Instantiate(prefab);
-			g.transform.SetParent(GameController.Instance.healthBarsParent.transform);
-            g.transform.GetChild(0).transform.position = Camera.main.WorldToScreenPoint(GetComponentInChildren<auxHealth>().gameObject.transform.position);
-            g1 = g.GetComponentInChildren<auxHealth>().gameObject;
-
-            visualHealth = g.GetComponentInChildren<auxHealth>().i;
+            auxMaxHealth = GetComponent<BuildingConstruction>().timer;
+            maxHealth = auxMaxHealth;
+            curHealth = maxHealth - GetComponent<BuildingConstruction>().timer;
+        }
+        else
+        {
+            maxHealth = GetComponent<Health>().getMaxHealth();
+            curHealth = GetComponent<Health>().getHealth();
         }
 
-		unitLOSEntity = gameObject.GetComponent<LOSEntity>();
+        g = Instantiate(prefab);
+        g.transform.SetParent(GameController.Instance.healthBarsParent.transform);
+        g.transform.GetChild(0).transform.position = Camera.main.WorldToScreenPoint(GetComponentInChildren<auxHealth>().gameObject.transform.position);
+        g1 = g.GetComponentInChildren<auxHealth>().gameObject;
+
+        visualHealth = g.GetComponentInChildren<auxHealth>().i;
+
+        unitLOSEntity = gameObject.GetComponent<LOSEntity>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (g == null)
+        if (g != null)
         {
-            if (GetComponent<Identity>().unitType.isBuilding())
-            {
-                //if (GetComponent<BuildingConstruction>().getPhase() == 2)
-                if (!GetComponent<BuildingConstruction>().getConstructionOnGoing())
-                {
-                    g = Instantiate(prefab);
-                    g.transform.SetParent(GameController.Instance.healthBarsParent.transform);
-                    g.transform.GetChild(0).transform.position = Camera.main.WorldToScreenPoint(GetComponentInChildren<auxHealth>().gameObject.transform.position);
-                    g1 = g.GetComponentInChildren<auxHealth>().gameObject;
 
-                    visualHealth = g.GetComponentInChildren<auxHealth>().i;
-                }
-                else
-                {
-                    Destroy(g);
-                    g = null;
-                }
-
-            }
-        }
-        else {
             g1.SetActive(true);
+
             g.transform.GetChild(0).transform.position = Camera.main.WorldToScreenPoint(GetComponentInChildren<auxHealth>().gameObject.transform.position);
 
-            auxHealth aH = g.GetComponentInChildren<auxHealth>();
+            //auxHealth aH = g.GetComponentInChildren<auxHealth>();
 
             maxXValue = g.GetComponentInChildren<auxHealth>().gray.rectTransform.position.x;
             minXValue = g.GetComponentInChildren<auxHealth>().gray.rectTransform.position.x - g.GetComponentInChildren<auxHealth>().gray.rectTransform.rect.width;
 
-            curHealth = GetComponent<Health>().getHealth();
-            HandleHealth();
 
-            if (g.GetComponentInChildren<auxHealth>().gray.rectTransform.position.y <= Screen.height * GameController.Instance.UIheight || unitLOSEntity.RevealState.Equals(LOSEntity.RevealStates.Hidden) ||
-                unitLOSEntity.RevealState.Equals(LOSEntity.RevealStates.Fogged))
-                g.GetComponentInChildren<auxHealth>().gameObject.SetActive(false);
-            
-
-
-                /*g1.SetActive(true);
-
-                if (g.GetComponentInChildren<auxHealth>().gray.rectTransform.position.y > Screen.height * GameController.Instance.UIheight)
+            if (GetComponent<Identity>().unitType.isBuilding())
+            {
+                //if (GetComponent<BuildingConstruction>().getPhase() == 2)
+                if (GetComponent<BuildingConstruction>().getConstructionOnGoing())
                 {
-
-                    g.GetComponentInChildren<auxHealth>().gameObject. SetActive(true);
-
-                    g.transform.GetChild(0).transform.position = Camera.main.WorldToScreenPoint(GetComponentInChildren<auxHealth>().gameObject.transform.position);
-
-                    maxXValue = g.GetComponentInChildren<auxHealth>().gray.rectTransform.position.x;
-                    minXValue = g.GetComponentInChildren<auxHealth>().gray.rectTransform.position.x - g.GetComponentInChildren<auxHealth>().gray.rectTransform.rect.width;
-
-                    curHealth = GetComponent<Health>().getHealth();
-                    HandleHealth();
-
+                    maxHealth = auxMaxHealth;
+                    curHealth = maxHealth - GetComponent<BuildingConstruction>().timer;
                 }
                 else
                 {
-                    g.GetComponentInChildren<auxHealth>().gameObject.SetActive(false);
-                }*/
-
-
+                    maxHealth = GetComponent<Health>().getMaxHealth();
+                    curHealth = GetComponent<Health>().getHealth();
+                }
             }
+            else
+            {
+                curHealth = GetComponent<Health>().getHealth();
+            }
+
+            HandleHealth();
+
+
+            if (g.GetComponentInChildren<auxHealth>().gray.rectTransform.position.y <= Screen.height * GameController.Instance.UIheight || unitLOSEntity.RevealState.Equals(LOSEntity.RevealStates.Hidden) ||
+                unitLOSEntity.RevealState.Equals(LOSEntity.RevealStates.Fogged))
+                g1.SetActive(false);
+
+        }
+
     }
 
-   
+
 
     private void HandleHealth()
     {
-        
+
         float currentXValue = MapValues(curHealth, 0, maxHealth, minXValue, maxXValue);
-        
+
         visualHealth.rectTransform.position -= new Vector3(visualHealth.rectTransform.position.x - currentXValue, 0, 0);
 
 
         if (curHealth > maxHealth / 2)
-        {  
+        {
             visualHealth.color = new Color32((byte)MapValues(curHealth, maxHealth / 2, maxHealth, 255, 0), 255, 0, 255);
         }
         else
@@ -134,11 +120,13 @@ public class healthbar : MonoBehaviour
         return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 
+
     public void destroyBar()
     {
         Destroy(g);
         g = null;
     }
 }
+
 
 
