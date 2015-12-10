bbc59ed3a8ebd0f6e4b3e90cfaa71b684b8a684d
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2015-12-10 15:32:58 +0100

    Open And Close By Pressing O

---
+++ RTS Dev Project/Assets/OpenAndClose.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenAndClose : MonoBehaviour
+{
+    private Animator animator;
+    private bool open = false;
+    
+	void Start ()
+    {
+        animator = gameObject.GetComponentOrEnd<Animator>();    
+
+        
+	}
+	
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.O))
+        {
+            open = !open;
+            animator.SetBool("open", open);
+        }
+	}
+}
