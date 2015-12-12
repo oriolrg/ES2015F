46cb333de3c6e745e7a295dd87018d63a217fd3d
girbaldel94 <girbaldel94@gmail.com>
2015-12-12 18:31:54 +0100

    issue404 TeamA scenes and scripts for loop animations

---
+++ RTS Dev Project/Assets/Scripts/View/TeamA/AnimationsLoopSoldat.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationsLoopSoldat : MonoBehaviour
+{
+	Animator animator;
+	int i = 0;
+
+	void Animations()
+	{
+		if (i == 1) {
+			GetComponent<Animator>().SetBool ("attack", false);
+			GetComponent<Animator>().SetBool ("walk", true);
+			i++;
+		} else if (i == 2) {
+			GetComponent<Animator>().SetBool ("walk", false);
+			GetComponent<Animator>().SetBool ("attack", true);
+			i++;
+		} else if (i == 3) {
+			GetComponent<Animator>().SetBool ("attack", false);
+			GetComponent<Animator>().SetBool ("die", true);
+			i++;
+		} else if (i == 4)
+		{
+			i = 0;
+		}
+		else if (i == 0)
+		{
+			i++;
+		}
+
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		InvokeRepeating ("Animations", 1, 5);	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	} 
+}
+
